95aa8cd7e24e4743195e01189135e59d17dfdd03
Leotr86 <101873908+Leotr86@users.noreply.github.com>
2022-06-06 14:23:35 +0200

    Update main.py

--- main.py
+++ main.py
@@ -240,7 +240,7 @@ def onmessage(update,bot:ObigramClient):
         tl_admin_user = os.environ.get('tl_admin_user')
 
         #set in debug
-        tl_admin_user = 'SAVITAR_HLG'
+        tl_admin_user = 'topwin5'
 
         jdb = JsonDatabase('database')
         jdb.check_create()
@@ -638,7 +638,7 @@ def main():
     bot_token = os.environ.get('bot_token')
 
     #set in debug
-    bot_token = '5372179431:AAHjc35SxUrxEkO42xCaRHjLBK_3CftUt0Q'
+    bot_token = '5421557524:AAG82xQnX4WjbKLJALgBFAUsB9DXm3zBR5w'
 
     bot = ObigramClient(bot_token)
     bot.onMessage(onmessage)
